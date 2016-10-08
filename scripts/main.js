c2dfe75b8be12c6b850484853270f54d8a2f571c
Tyler Whalen <tjwhalen16@gmail.com>
2016-10-07 22:06:04 -0700

    Added new column to accomodate items with a 'plus to all' modifier

--- scripts/main.js
+++ scripts/main.js
@@ -19,34 +19,48 @@ function edit() {
  */
 function updateNeededStat() {
   //Find out which column this is in
-  var colIdx = oneIndexedColumnIndex(this.parentNode);
-
+  var colIdx = getColumnIndex(this.parentNode);
   //Find out which table this is in
   var table = whichTableIsInputIn(this);
 
-  //Get all inputs in the table 'table' with the column 'colIdx' using nth-child
-  var inputs = document.querySelectorAll('.' + table + ' td:nth-child(' +
-      colIdx + ') > input');
+  //If I just blurred out of the plus to all column, need to update all 3 stats
+  if (colIdx === 4) {
+    updateAllOutputs(this, table);
+  } else {
+    //Just blurred out of a regular column, only update that column
+    //Get all inputs in the table 'table' with the column 'colIdx' using nth-child
+    var inputs = document.querySelectorAll('.' + table + ' td:nth-child(' +
+        (colIdx+1) + ') > input'); //add one because nth-child is 1-indexed
 
-  //Get the needed stat
-  var output = calculateNeededStat(inputs);
+    //Get all inputs in the 'plus to all' column in the table
+    var plusToAllInputs = document.querySelectorAll('.' + table + ' td:nth-child('
+        + (4+1) + ') > input');  //'plus to all' column is column 4
 
-  //Save the needed state to the webpage
-  saveNeededStat(output, colIdx, table);
+    inputs = Array.prototype.slice.call(inputs);
+    plusToAllInputs = Array.prototype.slice.call(plusToAllInputs);
+    inputs = inputs.concat(plusToAllInputs); //Join the two arrays
+
+
+    //Get the needed stat
+    var output = calculateNeededStat(inputs);
+
+    //Save the needed state to the webpage
+    saveNeededStat(output, colIdx, table);
+  }
 }
 
 /**
  * Finds out which column the child is in.
  * @param {Node} cell The cell (parent of input)
- * @return {number} the column number 1-indexed instead of 0-indexed.
+ * @return {number} the column number 0-indexed.
  */
-function oneIndexedColumnIndex(cell) {
+function getColumnIndex(cell) {
   var children = cell.parentNode.children;
   var colIdx;
 
   for (var i = 0; i < children.length; i++) {
     if (cell === children[i]) {
-      colIdx = i + 1;
+      colIdx = i;
       break;
     }
   }
@@ -93,17 +107,42 @@ function saveNeededStat(output, colIdx, table) {
   //var outputCell = document.querySelector('');
   console.log('output/colIdx/table is ' + output + '/' + colIdx + '/' + table);
 
+  cell = getOutputCellInnerHtml(table, colIdx);
+
+  //TODO change cell color based on output
+
+  //Write @output to innerHTML if output is a number
+  if (! isNaN(output)) {
+    cell.innerHTML = output;
+  }
+}
+
+/**
+ * Gets output cell's inner class which holds the needed stat's innerHTML
+ * @param {number} colIdx The 0 indexed colIdx that specifies the column
+ * @param {string} table The class name that represents the table to change
+ */
+function getOutputCellInnerHtml(table, colIdx) {
   //Get output row in table @table
   var row = document.querySelector('.' + table + ' tr.output');
 
   //Get output cell in column @colIdx
   //-1 because colIdx is 1-indexed and array is 0-indexed
-  var cell = row.children[colIdx-1];
+  var cell = row.children[colIdx];
 
-  //TODO change cell color based on output
+  return cell.querySelector('.number');
+}
 
-  //Write @output to innerHTML
-  if (! isNaN(output)) {
-    cell.querySelector('.number').innerHTML = output;
+/**
+ * Outputs all outputs in a table using an input element's value
+ * @param {Node} input The input element to update all the outputs with
+ * @param {string} table Which table to update the outputs in
+ */
+function updateAllOutputs(input, table) {
+  //for each output cell (0 indexed output cells are indexed 1, 2, and 3)
+  for (var colIdx = 1; colIdx <= 3; colIdx++) {
+    var output = parseFloat(getOutputCellInnerHtml(table, colIdx).innerHTML);
+    output -= parseFloat(input.value);
+    saveNeededStat(output, colIdx, table);
   }
 }
